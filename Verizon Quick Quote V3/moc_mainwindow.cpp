5fb5ddc0ae963cde96189463fe301def69852d35
ghgfhsh <ghgfhsh@gmail.com>
2020-01-06 19:07:58 -0600

    Add files via upload

---
+++ Verizon Quick Quote V3/moc_mainwindow.cpp
@@ -0,0 +1,308 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.11.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../VerizonQuickQuoteV2/VerizonQuickQuote/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.11.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[43];
+    char stringdata0[1289];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 33), // "on_totalpriceEdit_editingFini..."
+QT_MOC_LITERAL(2, 45, 0), // ""
+QT_MOC_LITERAL(3, 46, 34), // "on_totalpriceEdit2_editingFin..."
+QT_MOC_LITERAL(4, 81, 34), // "on_totalpriceEdit3_editingFin..."
+QT_MOC_LITERAL(5, 116, 34), // "on_totalpriceEdit4_editingFin..."
+QT_MOC_LITERAL(6, 151, 34), // "on_totalpriceEdit5_editingFin..."
+QT_MOC_LITERAL(7, 186, 34), // "on_DownpaymentEdit_editingFin..."
+QT_MOC_LITERAL(8, 221, 35), // "on_DownpaymentEdit2_editingFi..."
+QT_MOC_LITERAL(9, 257, 35), // "on_DownpaymentEdit3_editingFi..."
+QT_MOC_LITERAL(10, 293, 35), // "on_DownpaymentEdit4_editingFi..."
+QT_MOC_LITERAL(11, 329, 35), // "on_DownpaymentEdit5_editingFi..."
+QT_MOC_LITERAL(12, 365, 11), // "mainFuncion"
+QT_MOC_LITERAL(13, 377, 29), // "on_totalpriceEdit_textChanged"
+QT_MOC_LITERAL(14, 407, 4), // "arg1"
+QT_MOC_LITERAL(15, 412, 30), // "on_totalpriceEdit2_textChanged"
+QT_MOC_LITERAL(16, 443, 30), // "on_totalpriceEdit3_textChanged"
+QT_MOC_LITERAL(17, 474, 30), // "on_totalpriceEdit4_textChanged"
+QT_MOC_LITERAL(18, 505, 30), // "on_totalpriceEdit5_textChanged"
+QT_MOC_LITERAL(19, 536, 30), // "on_DownpaymentEdit_textChanged"
+QT_MOC_LITERAL(20, 567, 31), // "on_DownpaymentEdit2_textChanged"
+QT_MOC_LITERAL(21, 599, 31), // "on_DownpaymentEdit3_textChanged"
+QT_MOC_LITERAL(22, 631, 31), // "on_DownpaymentEdit4_textChanged"
+QT_MOC_LITERAL(23, 663, 31), // "on_DownpaymentEdit5_textChanged"
+QT_MOC_LITERAL(24, 695, 28), // "on_phoneinclude_stateChanged"
+QT_MOC_LITERAL(25, 724, 29), // "on_phoneinclude2_stateChanged"
+QT_MOC_LITERAL(26, 754, 29), // "on_phoneinclude3_stateChanged"
+QT_MOC_LITERAL(27, 784, 29), // "on_phoneinclude4_stateChanged"
+QT_MOC_LITERAL(28, 814, 29), // "on_phoneinclude5_stateChanged"
+QT_MOC_LITERAL(29, 844, 34), // "on_fianancemaxEdit_editingFin..."
+QT_MOC_LITERAL(30, 879, 35), // "on_monthlyplanInput_editingFi..."
+QT_MOC_LITERAL(31, 915, 36), // "on_monthlyplanInput2_editingF..."
+QT_MOC_LITERAL(32, 952, 36), // "on_monthlyplanInput3_editingF..."
+QT_MOC_LITERAL(33, 989, 36), // "on_monthlyplanInput4_editingF..."
+QT_MOC_LITERAL(34, 1026, 36), // "on_monthlyplanInput5_editingF..."
+QT_MOC_LITERAL(35, 1063, 25), // "on_clearallButton_clicked"
+QT_MOC_LITERAL(36, 1089, 28), // "on_clearphonesButton_clicked"
+QT_MOC_LITERAL(37, 1118, 27), // "on_DPIinput_editingFinished"
+QT_MOC_LITERAL(38, 1146, 28), // "on_DPIinput2_editingFinished"
+QT_MOC_LITERAL(39, 1175, 28), // "on_DPIinput3_editingFinished"
+QT_MOC_LITERAL(40, 1204, 28), // "on_DPIinput4_editingFinished"
+QT_MOC_LITERAL(41, 1233, 28), // "on_DPIinput5_editingFinished"
+QT_MOC_LITERAL(42, 1262, 26) // "on_action_Save_2_triggered"
+
+    },
+    "MainWindow\0on_totalpriceEdit_editingFinished\0"
+    "\0on_totalpriceEdit2_editingFinished\0"
+    "on_totalpriceEdit3_editingFinished\0"
+    "on_totalpriceEdit4_editingFinished\0"
+    "on_totalpriceEdit5_editingFinished\0"
+    "on_DownpaymentEdit_editingFinished\0"
+    "on_DownpaymentEdit2_editingFinished\0"
+    "on_DownpaymentEdit3_editingFinished\0"
+    "on_DownpaymentEdit4_editingFinished\0"
+    "on_DownpaymentEdit5_editingFinished\0"
+    "mainFuncion\0on_totalpriceEdit_textChanged\0"
+    "arg1\0on_totalpriceEdit2_textChanged\0"
+    "on_totalpriceEdit3_textChanged\0"
+    "on_totalpriceEdit4_textChanged\0"
+    "on_totalpriceEdit5_textChanged\0"
+    "on_DownpaymentEdit_textChanged\0"
+    "on_DownpaymentEdit2_textChanged\0"
+    "on_DownpaymentEdit3_textChanged\0"
+    "on_DownpaymentEdit4_textChanged\0"
+    "on_DownpaymentEdit5_textChanged\0"
+    "on_phoneinclude_stateChanged\0"
+    "on_phoneinclude2_stateChanged\0"
+    "on_phoneinclude3_stateChanged\0"
+    "on_phoneinclude4_stateChanged\0"
+    "on_phoneinclude5_stateChanged\0"
+    "on_fianancemaxEdit_editingFinished\0"
+    "on_monthlyplanInput_editingFinished\0"
+    "on_monthlyplanInput2_editingFinished\0"
+    "on_monthlyplanInput3_editingFinished\0"
+    "on_monthlyplanInput4_editingFinished\0"
+    "on_monthlyplanInput5_editingFinished\0"
+    "on_clearallButton_clicked\0"
+    "on_clearphonesButton_clicked\0"
+    "on_DPIinput_editingFinished\0"
+    "on_DPIinput2_editingFinished\0"
+    "on_DPIinput3_editingFinished\0"
+    "on_DPIinput4_editingFinished\0"
+    "on_DPIinput5_editingFinished\0"
+    "on_action_Save_2_triggered"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      40,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  214,    2, 0x08 /* Private */,
+       3,    0,  215,    2, 0x08 /* Private */,
+       4,    0,  216,    2, 0x08 /* Private */,
+       5,    0,  217,    2, 0x08 /* Private */,
+       6,    0,  218,    2, 0x08 /* Private */,
+       7,    0,  219,    2, 0x08 /* Private */,
+       8,    0,  220,    2, 0x08 /* Private */,
+       9,    0,  221,    2, 0x08 /* Private */,
+      10,    0,  222,    2, 0x08 /* Private */,
+      11,    0,  223,    2, 0x08 /* Private */,
+      12,    0,  224,    2, 0x08 /* Private */,
+      13,    1,  225,    2, 0x08 /* Private */,
+      15,    1,  228,    2, 0x08 /* Private */,
+      16,    1,  231,    2, 0x08 /* Private */,
+      17,    1,  234,    2, 0x08 /* Private */,
+      18,    1,  237,    2, 0x08 /* Private */,
+      19,    1,  240,    2, 0x08 /* Private */,
+      20,    1,  243,    2, 0x08 /* Private */,
+      21,    1,  246,    2, 0x08 /* Private */,
+      22,    1,  249,    2, 0x08 /* Private */,
+      23,    1,  252,    2, 0x08 /* Private */,
+      24,    1,  255,    2, 0x08 /* Private */,
+      25,    1,  258,    2, 0x08 /* Private */,
+      26,    1,  261,    2, 0x08 /* Private */,
+      27,    1,  264,    2, 0x08 /* Private */,
+      28,    1,  267,    2, 0x08 /* Private */,
+      29,    0,  270,    2, 0x08 /* Private */,
+      30,    0,  271,    2, 0x08 /* Private */,
+      31,    0,  272,    2, 0x08 /* Private */,
+      32,    0,  273,    2, 0x08 /* Private */,
+      33,    0,  274,    2, 0x08 /* Private */,
+      34,    0,  275,    2, 0x08 /* Private */,
+      35,    0,  276,    2, 0x08 /* Private */,
+      36,    0,  277,    2, 0x08 /* Private */,
+      37,    0,  278,    2, 0x08 /* Private */,
+      38,    0,  279,    2, 0x08 /* Private */,
+      39,    0,  280,    2, 0x08 /* Private */,
+      40,    0,  281,    2, 0x08 /* Private */,
+      41,    0,  282,    2, 0x08 /* Private */,
+      42,    0,  283,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,   14,
+    QMetaType::Void, QMetaType::QString,   14,
+    QMetaType::Void, QMetaType::QString,   14,
+    QMetaType::Void, QMetaType::QString,   14,
+    QMetaType::Void, QMetaType::QString,   14,
+    QMetaType::Void, QMetaType::QString,   14,
+    QMetaType::Void, QMetaType::QString,   14,
+    QMetaType::Void, QMetaType::QString,   14,
+    QMetaType::Void, QMetaType::QString,   14,
+    QMetaType::Void, QMetaType::QString,   14,
+    QMetaType::Void, QMetaType::Int,   14,
+    QMetaType::Void, QMetaType::Int,   14,
+    QMetaType::Void, QMetaType::Int,   14,
+    QMetaType::Void, QMetaType::Int,   14,
+    QMetaType::Void, QMetaType::Int,   14,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_totalpriceEdit_editingFinished(); break;
+        case 1: _t->on_totalpriceEdit2_editingFinished(); break;
+        case 2: _t->on_totalpriceEdit3_editingFinished(); break;
+        case 3: _t->on_totalpriceEdit4_editingFinished(); break;
+        case 4: _t->on_totalpriceEdit5_editingFinished(); break;
+        case 5: _t->on_DownpaymentEdit_editingFinished(); break;
+        case 6: _t->on_DownpaymentEdit2_editingFinished(); break;
+        case 7: _t->on_DownpaymentEdit3_editingFinished(); break;
+        case 8: _t->on_DownpaymentEdit4_editingFinished(); break;
+        case 9: _t->on_DownpaymentEdit5_editingFinished(); break;
+        case 10: _t->mainFuncion(); break;
+        case 11: _t->on_totalpriceEdit_textChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 12: _t->on_totalpriceEdit2_textChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 13: _t->on_totalpriceEdit3_textChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 14: _t->on_totalpriceEdit4_textChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 15: _t->on_totalpriceEdit5_textChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 16: _t->on_DownpaymentEdit_textChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 17: _t->on_DownpaymentEdit2_textChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 18: _t->on_DownpaymentEdit3_textChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 19: _t->on_DownpaymentEdit4_textChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 20: _t->on_DownpaymentEdit5_textChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 21: _t->on_phoneinclude_stateChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 22: _t->on_phoneinclude2_stateChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 23: _t->on_phoneinclude3_stateChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 24: _t->on_phoneinclude4_stateChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 25: _t->on_phoneinclude5_stateChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 26: _t->on_fianancemaxEdit_editingFinished(); break;
+        case 27: _t->on_monthlyplanInput_editingFinished(); break;
+        case 28: _t->on_monthlyplanInput2_editingFinished(); break;
+        case 29: _t->on_monthlyplanInput3_editingFinished(); break;
+        case 30: _t->on_monthlyplanInput4_editingFinished(); break;
+        case 31: _t->on_monthlyplanInput5_editingFinished(); break;
+        case 32: _t->on_clearallButton_clicked(); break;
+        case 33: _t->on_clearphonesButton_clicked(); break;
+        case 34: _t->on_DPIinput_editingFinished(); break;
+        case 35: _t->on_DPIinput2_editingFinished(); break;
+        case 36: _t->on_DPIinput3_editingFinished(); break;
+        case 37: _t->on_DPIinput4_editingFinished(); break;
+        case 38: _t->on_DPIinput5_editingFinished(); break;
+        case 39: _t->on_action_Save_2_triggered(); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 40)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 40;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 40)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 40;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
